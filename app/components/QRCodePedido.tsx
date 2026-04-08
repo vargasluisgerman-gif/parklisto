3fe1c3ca42c251781e83cae967bf3940c2e2654c
unknown <vargas.luis.german@gmail.com>
2026-04-07 21:20:49 -0300

    fix:corregir import qrcode.react

--- app/components/QRCodePedido.tsx
+++ app/components/QRCodePedido.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import QRCode from "qrcode.react";
+import { QRCodeCanvas } from "qrcode.react";
 
 export default function QRCodePedido({ pedidoId }: { pedidoId: number }) {
-  const url = `http://localhost:3000/pedidos/${pedidoId}`;
+  const url = `${window.location.origin}/pedidos/${pedidoId}`;
 
   return (
     <div style={{ marginTop: 10 }}>
-      <QRCode value={url} size={128} />
+      <QRCodeCanvas value={url} size={128} />
 
       <p style={{ fontSize: 12, marginTop: 5 }}>
         Escanear para ver pedido
